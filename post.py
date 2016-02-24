f88135475a58429c56dbb106822dbf3ac6b769a5
calvillain <hobbes102@gmail.com>
2016-02-24 13:54:43 -0500

    no message

--- post.py
+++ post.py
@@ -1,9 +1,9 @@
 from socket import *
-serverName = '192.168.1.5'
+serverName = '192.168.1.6'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-sentence = input('Input lowercase sentence:')
+sentence = raw_input('Input lowercase sentence:')
 clientSocket.send(sentence.encode('utf-8'))
 modifiedSentence = clientSocket.recv(1024)
 print( "From Server:")
