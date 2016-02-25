96d1dca27a2cf825a8a644f8387cf7c7cccb236c
calvillain <hobbes102@gmail.com>
2016-02-25 05:44:59 -0500

    updated post.py

--- post.py
+++ post.py
@@ -1,9 +1,18 @@
 from socket import *
 import sys, getopt, os
 
+verbose = True  #for debugging
+
+def vprint(arg):
+    #for debugging
+    if verbose:
+        print arg
+
 def printUsage():
     #prints usage instructions
     print 'usage: python post.py [-h hostname(optional)] [-p port#(optional)] [groupname(REQUIRED)]'
+    print 'type "quit" (without quotes) and press enter to end message.'
+
 
 def getUserId():
 	#returns user id
@@ -15,51 +24,82 @@ def main(argv):
     userId = getUserId()
     serverName = '192.168.1.6'    #default hostname
     serverPort = 12000          #default port number
-    print "user ID: ", userId
 
     #parsing arguments/options for hostname, port#
     try:
-        opts, args = getopt.getopt(argv,"h:p:")
+        opts, args = getopt.getopt(argv,"h:p:") #
         if len(sys.argv) not in (2,4,6,):   #must have 2, 4, or 6 option/argument tokens
-            print 'error: invalid command'
+            print 'error: invalid arguments'
             printUsage()
             sys.exit(1)
     except getopt.GetoptError:
-        print 'error: invalid command'
+        print 'error: invalid arguments'
         printUsage()
         sys.exit(1)
 
     #changing hostname/port to argument options
     for opt, arg in opts:
         if opt == '-h':
-            print "hostname: ", arg
             serverName = arg
-
         elif opt == '-p':
-            print "port#: ", arg
             serverPort = int(arg)
-        else:   #if option is neither -h nor -p
-            print 'error: invalid command'
-            printUsage()
-            sys.exit(1)
 
     #getting groupname from appropriate last argument array element
     groupName = sys.argv[ len(sys.argv) - 1 ]
 
-    print serverName, ": ", serverPort
-    print "group name: ", groupName
-    print "user id: ", userId
-
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName,serverPort))
 
-    post = "post "+groupName
-    print "post message: ", post
-    clientSocket.send(post.encode('utf-8'))
-    modifiedSentence = clientSocket.recv(1024)
-    print( "From Server:")
-    print(modifiedSentence)
-    clientSocket.close()
+    #********************************************#
+    vprint( 'host: '+serverName+": "+str(serverPort) )
+    vprint( "group name: "+groupName )
+    vprint( "user id: "+userId )
+    #********************************************#
+
+    group = "post "+groupName    #post msg with group name
+
+    clientSocket.send(group)
+    reply = clientSocket.recv(1024)
+    vprint( "From Server: "+reply)     #debug
+
+    if reply == 'ok':
+        vprint( 'group is ok! continuing with id...')   #debug
+        username = 'id '+userId
+        clientSocket.send(username)
+        reply2 = clientSocket.recv(1024)
+        vprint( "from Server: "+reply2)    #debug
+
+        if reply2 == 'ok':
+            vprint( 'id is ok! continuing with msg...') #debug
+
+            msg = []
+            sentinel = 'quit'   #type quit and press enter to exit message
+            for line in iter(raw_input, sentinel):
+                    clientSocket.send(line)
+            clientSocket.close()
+            sys.exit(0)
+
+        elif reply2.startswith('error'):
+            vprint( reply2 )     #debug
+            clientSocket.close()
+            sys.exit(1)
+        else:
+            print "server error"
+            clientSocket.close()
+            sys.exit(1)
+
+
+    elif reply.startswith('error'):
+        vprint( reply )  #debug
+        clientSocket.close()
+        sys.exit(1)
+    else:
+        print "server error"
+        clientSocket.close()
+        sys.exit(1)
+
+
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
